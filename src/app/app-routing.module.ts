7bb4651da0c55a7e64c101e6e522e61a3a2ea24c
caoyucong <whosmeya@gmail.com>
2019-07-25 21:50:10 +0800

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
 import { CenterComponent } from './pages/center/center.component';
+import { TestComponent } from './pages/test/test.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'center', component: CenterComponent },
+  { path: 'test', component: TestComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
+import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CenterComponent } from './pages/center/center.component';
+import { TestComponent } from './pages/test/test.component';
 @NgModule({
   declarations: [
     HomeComponent,
     CenterComponent,
-    AppComponent
+    TestComponent,
+    AppComponent,
   ],
   imports: [
     BrowserModule,
